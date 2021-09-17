1607d74b7021823341da6f4dba79fa5dbbd941fb
James Henderson <james@jarohen.dev>
2021-09-17 11:59:35 +0100

    running NVD through `lein run` rather than the lein profile now, see #1611

--- project.clj
+++ project.clj
@@ -95,8 +95,10 @@
                                                  "-Dxtdb.azure.blobs.test-container=crux-azure-blobs-test-container"]}
              :with-google-cloud-storage-test {:jvm-opts ["-Dxtdb.google.cloud-storage-test.bucket=crux-gcs-test"]}
              :with-chm-add-opens {:jvm-opts ["--add-opens" "java.base/java.util.concurrent=ALL-UNNAMED"]}
-             :nvd {:dependencies [[lein-nvd "1.5.0"]]
-                   :plugins [[lein-nvd "1.5.0"]]}}
+             :nvd {:source-paths ^:replace []
+                   :test-paths ^:replace []
+                   :dependencies ^:replace [[lein-nvd "1.5.0"]]
+                   :plugins ^:replace [[lein-nvd "1.5.0"]]}}
   :nvd {:fail-threshold 11}
   :pedantic? :warn
   :global-vars {*warn-on-reflection* true}
